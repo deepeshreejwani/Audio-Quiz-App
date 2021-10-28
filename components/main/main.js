18c24a4ad1f62076d0c4a4f6a68e5afa3e3a2fa6
deepeshreejwani <52446808+deepeshreejwani@users.noreply.github.com>
2021-10-28 14:03:57 +0530

    Update main.js

--- components/main/main.js
+++ components/main/main.js
@@ -3,23 +3,23 @@ import { SafeAreaView,FlatList } from "react-native";
 import Menu from "./menu";
 const data = [
   { text: "General Knowledge", image: '../../../assets/gk.png', category: "9" },
-  { text: "Books", image: '../../assets/book.png', category: "10" },
-  { text: "Movies", image: "../../assets/movie.png", category: "11" },
-  { text: "Music", image: "../../assets/music.png", category: "12" },
-  { text: "Television", image: "../../assets/tv.png", category: "14" },
-  { text: "Video Games", image: "../../assets/game.png", category: "15" },
-  {
-    text: "Science & Nature",
-    image: "../../assets/nature.png",
-    category: "17",
-  },
-  { text: "Computers", image: "../../assets/computer.png", category: "18" },
-  { text: "Geography", image: "../../assets/earth.png", category: "22" },
-  { text: "History", image: "../../assets/history.png", category: "23" },
-  { text: "Art", image: "../../assets/art.png", category: "25" },
-  { text: "Sport", image: "../../assets/sport.png", category: "21" },
-  { text: "Animals", image: "../../assets/animal.png", category: "27" },
-  { text: "Vehicles", image: "../../assets/car.png", category: "28" },
+//   { text: "Books", image: '../../assets/book.png', category: "10" },
+//   { text: "Movies", image: "../../assets/movie.png", category: "11" },
+//   { text: "Music", image: "../../assets/music.png", category: "12" },
+//   { text: "Television", image: "../../assets/tv.png", category: "14" },
+//   { text: "Video Games", image: "../../assets/game.png", category: "15" },
+//   {
+//     text: "Science & Nature",
+//     image: "../../assets/nature.png",
+//     category: "17",
+//   },
+//   { text: "Computers", image: "../../assets/computer.png", category: "18" },
+//   { text: "Geography", image: "../../assets/earth.png", category: "22" },
+//   { text: "History", image: "../../assets/history.png", category: "23" },
+//   { text: "Art", image: "../../assets/art.png", category: "25" },
+//   { text: "Sport", image: "../../assets/sport.png", category: "21" },
+//   { text: "Animals", image: "../../assets/animal.png", category: "27" },
+//   { text: "Vehicles", image: "../../assets/car.png", category: "28" },
 ];
 
 export default function Main({ navigation }) {
